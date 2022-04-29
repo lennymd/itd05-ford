cb69598d0dfa2f133d8313a5c27ce7b9721dd931
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2022-04-29 11:40:21 +0200

    update p5 code

--- p5ble-tutorial/sketch.js
+++ p5ble-tutorial/sketch.js
@@ -4,16 +4,16 @@ const SERVICE_UUID = 'cee3e619-9134-407e-8110-9b3b17babab8';
 //   button: 'cee3e619-9134-407e-8110-9b3b17babab8',
 // };
 
-let ledCharacteristic, happyButtonCharacteristic;
-let happyIndicator = 0;
+let ledCharacteristic;
+let ledStatus = 0;
 let myBLE;
 
 function setup() {
   // Create a p5ble class
   myBLE = new p5ble();
 
-  console.log('Happy Indicator', happyIndicator);
-  myBLE.write(happyButtonCharacteristic, happyIndicator);
+  console.log('LED status', ledStatus);
+  myBLE.write(ledCharacteristic, ledStatus);
 
   // Create a 'Connect' button
   const connectButton = createButton('Connect');
@@ -24,14 +24,14 @@ function setup() {
 }
 
 function toggleLED() {
-  if (happyIndicator == 0) {
-    happyIndicator = 1;
+  if (ledStatus == 0) {
+    ledStatus = 1;
   } else {
-    happyIndicator = 0;
+    ledStatus = 0;
   }
 
-  console.log(happyIndicator);
-  myBLE.write(happyButtonCharacteristic, happyIndicator);
+  console.log(ledStatus);
+  myBLE.write(ledCharacteristic, ledStatus);
 }
 
 function connectToBle() {
@@ -43,18 +43,18 @@ function connectToBle() {
 function getCharacteristics(error, characteristics) {
   if (error) console.log('error: ', error);
   console.log('characteristics: ', characteristics);
-  [ledCharacteristic, happyButtonCharacteristic] = characteristics;
+  ledCharacteristic = characteristics[0];
   // Read the value of the first characteristic
-  myBLE.read(happyButtonCharacteristic, gotValue);
+  myBLE.read(ledCharacteristic, gotValue);
 }
 
 // A function that will be called once got values
 function gotValue(error, value) {
   if (error) console.log('error: ', error);
   console.log('value: ', value);
-  happyIndicator = value;
+  ledStatus = value;
   // After getting a value, call p5ble.read() again to get the value again
-  myBLE.read(happyButtonCharacteristic, gotValue);
+  myBLE.read(ledCharacteristic, gotValue);
 }
 
 function draw() {
