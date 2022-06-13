3d7d592e9d08d07aa7ff019c9554355b55a2f9c0
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2022-06-13 15:57:13 +0200

    create secondary sketch

--- Phase3/sketch2.js
+++ Phase3/sketch2.js
@@ -1,10 +1,12 @@
+// TODO take a picture and save it to the folder
 let faceapi;
 let detections = [];
 
 let video;
 let canvas;
 
-let happy_Indicator = 0;
+// TODO simplify to only use Indicator
+// happy_indicator lets the program know that you're happy for long enough
 let happy_count = 0;
 let happy_keeping = 0;
 let happy_keeping_2 = 0;
@@ -12,22 +14,27 @@ let happy_keeping_2 = 0;
 let pictureTaken = false;
 let timecount = 0;
 
-let emotion = 0;
-let happy = false;
+// Program is in one of three emotions:
 // 0 === neutral, 1 === happy, 2 === angry;
+// we can send numbers to arduino and have it do things that way.
 
+let emotion_mode = 0;
+
+// ? Why is happy special?
+let happy = false;
+
+let img_neutral, img_happy;
+let mirror_vid_neutral, mirror_vid_angry;
+
+// ? do we need this
 function preload() {
-  img_bg = loadImage('image/bg.jpg');
-  //img_frame = loadImage('image/frame.png');
-  //img_mirror = loadImage('image/mirror.png');
-  //img_mirrorBg = loadImage('image/bg_mirror.jpg');
+  // img_bg = loadImage('image/bg.jpg');
   img_neutral = loadImage('image/Neutral.png');
   img_happy = loadImage('image/Happy.png');
 }
 
 function setup() {
-  // frameRate(30);
-  canvas = createCanvas(200, 60);
+  // canvas = createCanvas(200, 60);
   canvas.id('canvas');
   //canvas.hide();
 
@@ -73,20 +80,20 @@ function gotFaces(error, result) {
     return;
   }
 
-  detections = result; //Now all the data in this detections: 全ての検知されたデータがこのdetectionの中に
+  detections = result; //Now all the data in this detections
   // console.log(detections);
 
-  clear(); //Draw transparent background;: 透明の背景を描く
-  //drawBoxs(detections);//Draw detection box: 顔の周りの四角の描画
-  //drawLandmarks(detections);//// Draw all the face points: 全ての顔のポイントの描画
-  drawExpressions(detections, 20, 250, 14); //Draw face expression: 表情の描画
+  clear(); //Draw transparent background
+  //drawBoxes(detections);//Draw detection box
+  //drawLandmarks(detections);
+  drawExpressions(detections, 20, 250, 14); //Draw face expression
 
-  faceapi.detect(gotFaces); // Call the function again at here: 認識実行の関数をここでまた呼び出す
+  faceapi.detect(gotFaces); // Call the function again at here
 }
 
-function drawBoxs(detections) {
+function drawBoxes(detections) {
   if (detections.length > 0) {
-    //If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    //If at least 1 face is detected
     for (f = 0; f < detections.length; f++) {
       let {_x, _y, _width, _height} = detections[f].alignedRect._box;
       stroke(44, 169, 225);
@@ -98,7 +105,7 @@ function drawBoxs(detections) {
 }
 
 // function drawLandmarks(detections){
-//   if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+//   if (detections.length > 0) {//If at least 1 face is detected
 //     for (f=0; f < detections.length; f++){
 //       let points = detections[f].landmarks.positions;
 //       for (let i = 0; i < points.length; i++) {
@@ -112,7 +119,7 @@ function drawBoxs(detections) {
 
 function drawExpressions(detections, x, y, textYSpace) {
   if (detections.length > 0) {
-    //If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    //If at least 1 face is detected
     let {neutral, happy, angry, sad, disgusted, surprised, fearful} =
       detections[0].expressions;
     // textFont('Helvetica Neue');
@@ -120,50 +127,33 @@ function drawExpressions(detections, x, y, textYSpace) {
     // noStroke();
     // fill(44, 169, 225);
 
+    // TODO only do counting here, no other math/calculation/action/etc.
+    // only increade happy_count if person is smiling continuously
     if (nf(happy) > 0.6) {
       happy_count++;
-      if (happy_count > 15 && happy_keeping == 0) {
-        happy_count = 0;
-        happy_keeping = 1;
-        happy_keeping_2 = 10;
-        happy_Indicator = 1;
-      } else {
-        happy_Indicator = 0;
-        happy_keeping = 0;
-      }
     } else {
       happy_count = 0;
-      happy_Indicator = 0;
-      happy_keeping = 0;
     }
-    //console.log(happy_Indicator);
-
-    // text("neutral:       " + nf(neutral*100, 2, 2)+"%", x, y);
-    // text("happiness: " + nf(happy*100, 2, 2)+"%", x, y+textYSpace);
-    // text("anger:        " + nf(angry*100, 2, 2)+"%", x, y+textYSpace*2);
-    // text("sad:            "+ nf(sad*100, 2, 2)+"%", x, y+textYSpace*3);
-    // text("disgusted: " + nf(disgusted*100, 2, 2)+"%", x, y+textYSpace*4);
-    // text("surprised:  " + nf(surprised*100, 2, 2)+"%", x, y+textYSpace*5);
-    // text("fear:           " + nf(fearful*100, 2, 2)+"%", x, y+textYSpace*6);
+    // console.log('happiness', nf(happy * 100, 2, 2) + '%');
   } else {
-    //If no faces is detected: 顔が1つも検知されていなかったら
-    text('neutral: ', x, y);
-    text('happiness: ', x, y + textYSpace);
-    text('anger: ', x, y + textYSpace * 2);
-    text('sad: ', x, y + textYSpace * 3);
-    text('disgusted: ', x, y + textYSpace * 4);
-    text('surprised: ', x, y + textYSpace * 5);
-    text('fear: ', x, y + textYSpace * 6);
+    //If no faces is detected
+    // text('neutral: ', x, y);
+    // text('happiness: ', x, y + textYSpace);
+    // text('anger: ', x, y + textYSpace * 2);
+    // text('sad: ', x, y + textYSpace * 3);
+    // text('disgusted: ', x, y + textYSpace * 4);
+    // text('surprised: ', x, y + textYSpace * 5);
+    // text('fear: ', x, y + textYSpace * 6);
   }
 }
 
 function keyPressed() {
   if (keyCode === 78) {
-    emotion = 0;
+    emotion_mode = 0;
   } else if (keyCode === 72) {
-    emotion = 1;
+    emotion_mode = 1;
   } else if (keyCode === 65) {
-    emotion = 2;
+    emotion_mode = 2;
   }
 }
 
@@ -172,33 +162,54 @@ function draw() {
   if (happy == true) {
     mirror_buddy = image(img_happy, 0, 0, 120, 60);
   }
-  if (emotion == 1) {
+  if (emotion_mode == 1) {
     //HAPPY CODE
+    // Check if person has been smiling for long enough
+    if (happy_count > 10) {
+      // Make the buddy smile
+      // Take a picture if you can
+    }
+    // Check if person has stopped smiling
+    if (happy_count == 0) {
+    // Set a timer to transition to neutral face in happy mode.
+
+    // Check if you've been happy for 5 seconds
+    // Take a picture if you've been happy for 30 seconds or so
     if (pictureTaken) {
-      if (frameCount % 60 == 0) {
-        timecount++;
-        happy_keeping_2 = happy_keeping_2 - 1;
-        //console.log(timecount);
-        console.log(happy_keeping_2);
-        if (timecount >= 5 && happy_keeping_2 < 5) {
-          pictureTaken = false;
-          timecount = 0;
-          console.log("I'm neutral now");
-          happy = false;
-        }
+      // if (frameCount % 60 == 0) {
+      //   timecount++;
+      //   happy_keeping_2 = happy_keeping_2 - 1;
+      //   //console.log(timecount);
+      //   console.log(happy_keeping_2);
+      //   if (timecount >= 5 && happy_keeping_2 < 5) {
+      //     pictureTaken = false;
+      //     timecount = 0;
+      //     console.log("I'm neutral now");
+      //     happy = false;
+      //   }
+      // }
+      if (happy_count > 15) {
+        //   happy_count = 0;
+        //   happy_keeping = 1;
+        //   happy_keeping_2 = 10;
+        //   happy_Indicator = 1;
+        // } else {
+        //   happy_Indicator = 0;
+        //   happy_keeping = 0;
       }
     } else {
-      if (happy_Indicator) {
+      // Picture has not been taken. If the person is happy enough, take a picture and then start the maintain happy timer.
+      if (happy_count > 15) {
         console.log("I'm happy now");
         happy = true;
         pictureTaken = true;
         happy_keeping_2 = 10;
       }
     }
-  } else if (emotion == 2) {
+  } else if (emotion_mode == 2) {
     //ANGRY CODE
     mirror_buddy = image(mirror_vid_angry, 0, 0, 120, 60);
-  } else if (emotion == 0) {
+  } else if (emotion_mode == 0) {
     //NEUTRAL CODE
     mirror_buddy = image(mirror_vid_neutral, 0, 0, 120, 60);
   }
