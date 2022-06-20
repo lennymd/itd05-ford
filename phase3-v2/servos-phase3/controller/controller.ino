d249a7f069cb760729f7c58d6be195f4d6540d66
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2022-06-20 20:24:54 +0200

    make phase3 self-sustained

--- phase3-v2/servos-phase3/controller/controller.ino
+++ phase3-v2/servos-phase3/controller/controller.ino
@@ -2,54 +2,103 @@
 #include <Servo.h>
 
 // create servo variables
-Servo servo_left;
-Servo servo_center;
-Servo servo_right;
 
-char incomingByte; 
+Servo servoL;
+Servo servoC;
+Servo servoR;
 
+// we'll read the out_message from p5 into this incomingByte using the serial connection
+char incoming_byte = '1';
 
+//posL is for the left servo
+int posL = 0;
+int posC = 0;
+int posR = 0;
 
 void setup() {
   Serial.begin(9600);
-  
-  // set up Servo to work 
-  servo_left.attach(2);
-  servo_center.attach(3);
-  servo_right.attach(4)
-  
 
-  // 2 is calibrated as neutral servo state. This is happy state.
-  buddyServo.write(2);
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  // set up Servo to work. they should be plugged into D2,D3,D4 from Left (steering wheel) to right (empty vacuum).
+  //  servoL.attach(2);
+  //  servoC.attach(3);
+  //  servoR.attach(4);
+  //
+  //  servoL.write(0);
+  //  servoC.write(0);
+  //  servoR.write(0);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  //  neutral idea
+  //  turnServo(servoL, 0, 180, 5, 15);
+  //  delay(1000);
+  //  turnServo(servoC, 0, 180, 5, 15);
+  //  delay(1000);
+  //  turnServo(servoR, 0, 180, 5, 15);
+  //  delay(2000);
+
+  //  turnServo(servor, 180, 0, 5, 15);
+  //  delay(1000);
+  //  turnServo(servoC, 180, 0, 5, 15);
+  //  delay(1000);
+  //  turnServo(servoL, 180, 0, 5, 15);
+  //  delay(1000);
+
   if (Serial.available() > 0) {
-    //Serial.println("test");
-    // read the oldest byte in the serial buffer:
-    incomingByte = Serial.read();
-    //Serial.println(incomingByte);
-    if (incomingByte == '1') {
-      // neutral state. Set LED to 0. And Neutral position for servo
-      buddyServo.write(2);
-      leds.setColorRGB(0, 0,0,0);
-    }
+    // if there is a serial port available that we can communicate with, do stuff.
 
-    if (incomingByte == '2'){
-      // happy state. Set LED to 255,0,0. Rotate to position angry (180)
-      buddyServo.write(180);
-      leds.setColorRGB(0,255,0,0);      
-    }
-
-    if (incomingByte == '3'){
-      // happy state. SET LEd to 0,255,0. Make sure position is happy (2)
-      buddyServo.write(2);
-      leds.setColorRGB(0,0,255,0);
-    }
+    // read the oldest byte in the buffer.
+    incoming_byte = Serial.read();
+    Serial.println(incoming_byte);
+  }
+  // check what incoming_byte is and then do the right rotations.
+  if (incoming_byte == '1') {
+    // neutral state
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
   }
 
+  if (incoming_byte == '2') {
+    // happy state
+    digitalWrite(LED_BUILTIN, HIGH);
+    //            delay(1000);
+    //            digitalWrite(LED_BUILTIN, LOW);
+    //            delay(1000);
+  }
+
+  if (incoming_byte == '3') {
+    // angry state
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+  }
+}
+
+
+
+void turnServo(Servo ser, int startPos, int endPos, int sStep, int sDelay) {
+  int _pos = 0;
+  for (_pos = startPos; _pos <= endPos; _pos += sStep) {
+    ser.write(_pos);
+    delay(sDelay);
+  }
+
+  //  for (_pos = endPos; _pos >= startPos; _pos += sStep) {
+  //    ser.write(_pos);
+  //    delay(sDelay);
+  //  }
 }
